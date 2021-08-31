6d9c9ab63a4e7fe889925f10fda79e3842258331
Jarne <jarne.staal9@gmail.com>
2021-08-31 13:55:20 +0200

    Voordelen added

--- src/components/Home/VoordelenCoaching.vue
+++ src/components/Home/VoordelenCoaching.vue
@@ -1,28 +1,52 @@
 <template>
-<div>
-  <h1 class="mt-5">Voordelen van coaching</h1>
+<div class="fullSection">
 
-  <div class="d-flex justify-content-center">
-    <div>
-      <i class="far fa-flag"></i>
-      <h2>Voordeel 1</h2>
-      <p>Je bereikt je doelen sneller</p>
-    </div>
+  <div class="desktopVersionVoordelen">
+    <h1 class="mt-5">Voordelen van coaching</h1>
 
-    <div>
-      <i class="fas fa-eye"></i>
-      <h2>Voordeel 2</h2>
-      <p>Je bereikt je doelen sneller</p>
-    </div>
+    <div class="d-flex justify-content-evenly mt-lg-5">
+      <div>
+        <font-awesome-icon :icon="['far', 'flag']" />
+        <h2>Voordeel 1</h2>
+        <p>Je bereikt je doelen sneller</p>
+      </div>
 
-    <div>
-      <i class="fas fa-male"></i>
-      <h2>Voordeel 3</h2>
-      <p>Je bereikt je doelen sneller</p>
+      <div>
+        <font-awesome-icon :icon="['fas', 'eye']" />
+        <h2>Voordeel 2</h2>
+        <p>Blinde vlekken worden zichtbaar</p>
+      </div>
+
+      <div>
+        <font-awesome-icon :icon="['fas', 'male']" />
+        <h2>Voordeel 3</h2>
+        <p>Je wordt zelfredzamer</p>
+      </div>
     </div>
   </div>
+  <div class="mobileVersionVoordelen">
+    <h1 class="mt-5 mb-5">Voordelen van coaching</h1>
 
+    <div class="d-flex justify-content-evenly mt-xl-5 containerVoordelen">
+      <div class="mb-4">
+        <font-awesome-icon :icon="['far', 'flag']" />
+        <h2>Voordeel 1</h2>
+        <p>Je bereikt je doelen sneller</p>
+      </div>
 
+      <div class="mb-4">
+        <font-awesome-icon :icon="['fas', 'eye']" />
+        <h2>Voordeel 2</h2>
+        <p>Blinde vlekken worden zichtbaar</p>
+      </div>
+
+      <div class="mb-4">
+        <font-awesome-icon :icon="['fas', 'male']" />
+        <h2>Voordeel 3</h2>
+        <p>Je wordt zelfredzamer</p>
+      </div>
+    </div>
+  </div>
 </div>
 </template>
 
@@ -32,11 +56,37 @@ export default {
 }
 </script>
 
+
 <style scoped>
+.mobileVersionVoordelen {
+  display: none;
+}
+
 * {
   font-family: 'Open Sans', sans-serif;
 }
 h1 {
   font-weight: bold;
+}
+svg {
+  font-size: 35px !important;
+}
+
+.fullSection {
+  margin-top: 5em;
+}
+
+@media only screen and (max-width: 992px){
+  .desktopVersionVoordelen {
+    display: none;
+  }
+
+  .mobileVersionVoordelen {
+    display: block;
+  }
+  .containerVoordelen {
+    flex-direction: column !important;
+  }
+
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import { createApp } from 'vue'
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome"
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {library} from "@fortawesome/fontawesome-svg-core"
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import {far} from '@fortawesome/free-regular-svg-icons'
 
-createApp(App).use(store).use(router).mount('#app')
+library.add(fas);
+library.add(far);
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .use(store)
+    .use(router)
+    .mount('#app')
